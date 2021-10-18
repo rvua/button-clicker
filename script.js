38ba6baf6650bde7acf09ac80ca1436fc4d170eb
rvua <rvanore26@gmail.com>
2021-10-18 17:32:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Change the text to say "Logout" when clicking the "Login" button
+function logOut(element){
+    element.innerHTML = 'Log Out';
+}
+
+// Remove the Add Definition button when it is clicked 
+function remove(element){
+    element.remove();
+}
+
+// Alert "Ninja was liked" when clicking a like button
+function alertLiked(){
+    alert("Ninja was liked");
+}
